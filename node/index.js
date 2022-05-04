c4d47ea28e6c24cda75a42c7898120ae67139fb5
lorenzotelo03 <lorenzotelo03@gmail.com>
2022-05-04 23:05:12 +0000

    dockerized node app and minor changes

--- node/index.js
+++ node/index.js
@@ -1,20 +1,44 @@
-const mysql = require('mysql');
+'sue strict';
+var app = require('express')();
+var mysql = require('mysql');
+var { body, validationResult } = require('express-validator');
 
-var mysqlConnection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'lore',
-        password: 'lore',
-        database: 'db'
-    }
-);
+var port = process.env.PORT || 8005;
+var responseStr = "MySQL Data";
 
-mysqlConnection.connect((err)=>{
-        if(!err){
-            console.log("Db connection was succesfull");
+app.get('/',function(req,res){
 
-        }else{
-            console.log("Db connection failed");
-        }
-    }
-);
+    var mysqlHost = process.env.MYSQL_HOST || '127.0.0.1';
+    var mysqlPort = process.env.MYSQL_PORT || '3306';
+    var mysqlUser = process.env.MYSQL_USER || 'lore';
+    var mysqlPass = process.env.MYSQL_PASS || 'lore';
+    var mysqlDB = process.env.MYSQL_DB || 'db';
+    
+    var connectionOptions = {
+        host: mysqlHost,
+        port: mysqlPort,
+        user: mysqlUser,
+        password: mysqlPass,
+        database: mysqlDB
+    };
+    console.log('MySQL connection config:');
+    console.log(connectionOptions);
+
+    var connection = mysql.connect(connectionOptions);
+    connection.connect();
+    /*
+    *connection.query(qua scrivi la query){
+        if(error)throw error;
+
+       ? responseStr = '';
+        results.forEach(function(data){
+            .....
+        });
+       * if(responseStr.lenght == 0) response = "no records found";
+        console.log(responseStr);
+
+        res.status(200).send(responseùStr)
+    };
+    */
+    connection.end();
+});

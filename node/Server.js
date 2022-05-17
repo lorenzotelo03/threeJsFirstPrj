209120f83d38b7f3294b257b8f1586a5239fef22
lorenzotelo03 <lorenzotelo03@gmail.com>
2022-05-17 22:04:58 +0000

    added some things? i guess

--- node/Server.js
+++ node/Server.js
@@ -8,6 +8,7 @@ const mysql = require('mysql');
 const fs = require('fs');
 const bcrypt = require('bcrypt');//for hash algorithm
 var nodemailer = require('nodemailer');//to send emails
+var multer = require('multer');
 const router = express.Router();
 
 // var { body, validationResult } = require('express-validator');
@@ -33,29 +34,42 @@ var connection = mysql.createConnection(connectionOptions);
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: '3dSpaceTeam@gmail.com',
-      pass: '3dSpaceTeam'
+      user: '3dSpaceCrew@gmail.com',
+      pass: '3dSpaceCrew0099'
     }
 });
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 //crea una sessione
 app.use(session({
 	secret: 'secret',
 	resave: false,
 	saveUninitialized: false
 }));
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '/models/' + req.session.username))
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
 
+//will be using this for uplading
+const upload = multer({ storage: storage });
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/Html/home.html'));
     //__dirname : It will resolve to your project folder.
 });
+
 app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, '/Html/login.html'));
     //__dirname : It will resolve to your project folder.
 });
+
 app.get('/3dWorkSpace', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Html/index.html'));
+  res.sendFile(path.join(__dirname, '/3dWorkSpace.html'));
 });
 
 router.get('/changePass',function(req,res){
@@ -93,7 +107,7 @@ app.post('/auth',function(req,res){ //login function
 
             if(results.length > 0){
               req.session.loggedIn = true;
-              req.session.username = username;             
+              req.session.username = username;          
               return res.redirect('/3dWorkSpace');
               //res.send("you're in congrats");
             }else{
@@ -120,8 +134,7 @@ app.post('/regi',function(req,res){ //sing in function
   if (username && email && password){ //check if the inputs filed are not empty
     connection.query("SELECT * FROM `Utenti` WHERE UserName= ? and Email = ?",[username,email], function(error, results, fields){
       if (error) {
-            console.log(error);
-            res.sendStatus(500);
+            res.status(500).send(error);
             return;
         }
       Object.keys(results).forEach(function(key){
@@ -135,7 +148,7 @@ app.post('/regi',function(req,res){ //sing in function
       connection.query("INSERT INTO Utenti (Email,UserName, uPassword) VALUES (?,?,?);" , [email, username, hashedP], function(error, results, fields){ 
         if (error) {
             console.log(error);
-            res.sendStatus(500);
+           res.status(500).send(error);
             return;
         }
         
@@ -158,81 +171,58 @@ app.post('/chPas',function(req,res){
   let isValid = false;
   console.log(email);
   connection.query("SELECT Email FROM `Utenti`" , function(error, results, fields){ 
-    console.log("we're in the query");
+
     if (error) {
           console.log(error);
           res.sendStatus(500);
           return;
-    }else{
-      Object.keys(results).forEach(function(key){
-          let row = results[key];
-          console.log(row.Email);
-          if(row.Email == email){
-            isValid = true;
-            return true;
-          }
-      });
-      if(isValid){
-        let mailOptions = {
-          from: '3dSpaceTeam@gmail.com',
-          to: email,
-          subject: 'Password reset',
-          html: '<p>Click <a href="https://8005-lorenzotelo-threejsfirs-yftserqua8e.ws-eu45.gitpod.io/">here</a> to reset your password</p>'
-        }
-
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-      }
     }
+    console.log(results);
+    for(let i=0; i<results.length; i++){
+        let row = results[i];
+        console.log(email,"==",row.Email,":",email==row.Email);
+        if(row.Email == email){
+          console.log("valid email");
+          isValid = true;
+          break;
+        }
+    }
+    console.log("isValid : ",isValid);
+    if(!isValid){
+      res.status(400).send("email not found");
+      return;
+    }
+      console.log("inside your body");
+      let mailOptions = {
+        from: '3dSpaceTeam@gmail.com',
+        to: email,
+        subject: 'Password reset',
+        html: '<p>Click <a href="https://8005-lorenzotelo-threejsfirs-yftserqua8e.ws-eu45.gitpod.io/">here</a> to reset your password</p>',
+      };
+      transporter.sendMail(mailOptions);//, function(error, info){
+      //     if (error) {
+      //       console.log(error);
+      //       return;
+      //     } 
+      //     console.log('Email sent: ' + info.response);
+          
+      // });
+    });
   });
+
+app.post('/addModel',upload.single('file'),function(req,res){
+  if(!req.session.login) {
+    res.status(401).send("You're not logged in");
+    return;
+  }
+  if(!req.file){
+    res.status(500).send("internal server Error");
+  }
+    res.status(200).send("you've succesfully uploaded your file");
+
 });
 
-app.post('/chPas',function(req,res){
-  let email = req.body.email;
-  let isValid = false;
-  console.log(email);
-  connection.query("SELECT Email FROM `Utenti`" , function(error, results, fields){ 
-    console.log("we're in the query");
-    if (error) {
-          console.log(error);
-          res.sendStatus(500);
-          return;
-    }else{
-      Object.keys(results).forEach(function(key){
-          let row = results[key];
-          console.log(row.Email);
-          if(row.Email == email){
-            isValid = true;
-            return true;
-          }
-      });
-      if(isValid){
-        let mailOptions = {
-          from: '3dSpaceTeam@gmail.com',
-          to: email,
-          subject: 'Password reset',
-          html: '<p>Click <a href="https://8005-lorenzotelo-threejsfirs-yftserqua8e.ws-eu45.gitpod.io/">here</a> to reset your password</p>'
-        }
 
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
-      }
-    }
-  });
-});
 app.use(express.static(__dirname + '/Html'));
 app.use('/', router);
 app.listen(port);
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// });
